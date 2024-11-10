b760e2ea40c405f32ab98687aa638e995ad4a695
Hyeon Jong Kim <khje1027@gmail.com>
2024-11-11 02:59:55 +0900

    이제 자고 싶다

--- components/FirstVisitAlertMobile.jsx
+++ components/FirstVisitAlertMobile.jsx
@@ -19,7 +19,7 @@ export default function FirstVisitAlert() {
 
   return (
     <AlertDialog open={isAlertOpen} onOpenChange={setIsAlertOpen}>
-      <AlertDialogContent className="w-[90%] h-auto py-12 flex flex-col justify-between items-center rounded-xl">
+      <AlertDialogContent className="w-[200px] h-auto py-12 flex flex-col justify-between items-center rounded-xl">
         <div>
           <Image src="/images/resume.png" alt="Logo" width={192} height={68} />
         </div>
